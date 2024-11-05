1820d394d98a0bca813c7be9771fe046bca7d356
Siddiq05 <mustafasiddiq313@gmail.com>
2024-11-05 18:12:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const text = document.getElementById("text");
+const taskbtn = document.querySelector('.task-btn');
+const tasklist = document.querySelector('.task-list');
+
+
+
+
+const addtotask = () => {
+    if (text.value.trim() === '') {
+        alert('please enter a task')  
+    }
+    else{
+        const li = document.createElement('li');
+        li.innerHTML = `${text.value}`
+        tasklist.appendChild(li);
+        const span = document.createElement('span');
+        span.innerHTML = `<span>&times;</span>`
+        li.appendChild(span);
+
+        const remove = () => {
+            li.remove();
+            
+        }
+    
+        span.addEventListener('click', remove)
+    }
+    text.value = '';
+    
+    
+}
+
+
+
+
+taskbtn.addEventListener('click', addtotask)
+
